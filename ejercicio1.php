6e6bced1c6277cc7ecbec450d4a96020657f7613
Ester <ester.rivero.88@gmail.com>
2024-10-17 09:39:09 +0200

    Codigo con CSS

--- ejercicio1.php
+++ ejercicio1.php
@@ -2,30 +2,38 @@
 // Ejercicio 1
 // Escriba un programa que cada vez que se ejecute muestre la tirada de entre 1 y 10
 // dados al azar y diga el número de valores pares e impares obtenidos.
+
+// Función que indica el número de dados que se van tirar entre 1-10
 function generarNumeroTiradas() {
     return rand(1, 10);
 }
 
+// Función para tirar los dados con valor entre 1-6
 function realizarTiradas($numeroTiradas) {
     $contadorPares = 0;
     $resultados = [];
 
     for ($i = 0; $i < $numeroTiradas; $i++) {
         $tirada = rand(1, 6);
+        // Si es número par, suma 1
         if ($tirada % 2 == 0) {
             $contadorPares++;
         }
+        // El resultado se incluye en un array
         $resultados[] = $tirada;
     }
 
     return [$resultados, $contadorPares];
 }
 
+// Función para mostrar los resultados
 function mostrarResultados($resultados, $contadorPares) {
+    // Recorre el array resultados y por cada tirada se saca la imagen del dado que corresponde con su valor
     foreach ($resultados as $tirada) {
         echo "<img src='img/Dado $tirada.svg' alt='dado$tirada'>";
     }
 
+    // Contador de número de tiradas
     $numeroTiradas = count($resultados);
     echo "<p>Se han obtenido $contadorPares valores pares y " . ($numeroTiradas - $contadorPares) . " valores impares.</p>";
 }
@@ -37,7 +45,7 @@ echo "<h1>Se han realizado $numeroTiradas tiradas de dados</h1>";
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -46,7 +54,7 @@ echo "<h1>Se han realizado $numeroTiradas tiradas de dados</h1>";
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <h1>Resultado de la tirada</h1>
+    <h2>Resultado de la tirada</h2>
     <?php
     mostrarResultados($resultados, $contadorPares);
     ?>
